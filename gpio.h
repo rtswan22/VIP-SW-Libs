303ceab1b35bace78a0ca584852767e4cc51bddf
rtswan22 <46530898+rtswan22@users.noreply.github.com>
2020-03-10 09:43:59 -0400

    Update gpio.h

--- gpio.h
+++ gpio.h
@@ -24,6 +24,7 @@ void gpio_enable_input(unsigned int pins);
 unsigned int gpio_read_input(unsigned int pins);
 //// Output
 void gpio_enable_output(unsigned int pins, unsigned int pin_outputs);
+void gpio_set_output(unsigned int pins, unsigned int pin_outputs);
 //// Interrupts
 void gpio_enable_interrupt_posedge(unsigned int pins); //CHECK: are posedge and negedge mutually exclusive? should they be?
 void gpio_disable_interrupt_posedge(unsigned int pins); //CHECK: are posedge and negedge mutually exclusive? should they be?
