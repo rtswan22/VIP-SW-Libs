facadf419e7a2371cfa094f2718e5fcbcf216b1a
rtswan22 <46530898+rtswan22@users.noreply.github.com>
2020-04-01 13:25:14 -0400

    Update pwm_example.c

--- examples/pwm_example.c
+++ examples/pwm_example.c
@@ -1,8 +1,9 @@
 #include "../common.h"
 #include "../pwm.h"
+#include "../inc/ccode_macros.h"
 
 int main() {
   MAIN_INIT;
-  pwm_set_frequency(PWM_MAX_FREQ);
+  pwm_set_frequency(0, PWM_MAX_FREQ);
   MAIN_RETURN;
 }
