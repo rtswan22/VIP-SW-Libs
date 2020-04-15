88bdafd07faa6c794f6a7c7625eabedafe6be641
rtswan22 <46530898+rtswan22@users.noreply.github.com>
2020-04-15 13:38:38 -0400

    Update gpio_example.c

--- examples/gpio_example.c
+++ examples/gpio_example.c
@@ -6,14 +6,14 @@
 
 // 'digit' is aligned [][7:0] = [-, a, b, c, d, e, f, g] for the 7 segment display
 // 'digit': 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, b, C, d, E, f.
-unsigned char digit[] = {0b1111110, 0b01100000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1110011, 0b1110111, 0b0011111, 0b1001110, 0b0111101, 0b1001111, 0b1000111};
+unsigned int digit[] = {0b1111110, 0b01100000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1110011, 0b1110111, 0b0011111, 0b1001110, 0b0111101, 0b1001111, 0b1000111};
 
 int main() {
   MAIN_INIT;
   // Variables
   int digit_sel = 0; // 'digit' index
   int digit_count = 16; // 'digit' count
-  unsigned char pins_6_0 = GPION(6) | GPION(5) | GPION(4) | GPION(3) | GPION(2) | GPION(1) | GPION(0); // output pin mask
+  unsigned int pins_6_0 = GPION(6) | GPION(5) | GPION(4) | GPION(3) | GPION(2) | GPION(1) | GPION(0); // output pin mask
   // IO Config
   gpio_enable_output(pins_6_0, digit[digit_sel]); // gpio pins 6-0 are outputs
   gpio_enable_input(GPION(7)); // gpio pin 7 is an input
