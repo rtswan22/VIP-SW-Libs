7171b02004ee6e8aa1e6d1d011c23ce5edeac6e8
rtswan22 <46530898+rtswan22@users.noreply.github.com>
2020-04-20 11:44:52 -0400

    Update pwm_example.c

--- examples/pwm_example.c
+++ examples/pwm_example.c
@@ -1,39 +1,23 @@
 #include "../pwm.h"
 #include "../csim_macros.h"
 
-unsigned int melody_length = 80;
-unsigned int melody[] = { // source: https://www.princetronics.com/supermariothemesong/
-  2637, 2637, 0, 2637,
-  0, 2093, 2637, 0,
-  3136, 0, 0,  0,
-  1568, 0, 0, 0,
-
-  2093, 0, 0, 1568,
-  0, 0, 1319, 0,
-  0, 1760, 0, 1976,
-  0, 1865, 1760, 0,
-
-  1568, 2637, 3136,
-  3520, 0, 2794, 3136,
-  0, 2637, 0, 2093,
-  2349, 1976, 0, 0,
-
-  2093, 0, 0, 1568,
-  0, 0, 1319, 0,
-  0, 1760, 0, 1976,
-  0, 1865, 1760, 0,
-
-  1568, 2637, 3136,
-  3520, 0, 2794, 3136,
-  0, 2637, 0, 2093,
-  2349, 1976, 0, 0
-};
-
 int main() {
   MAIN_INIT;
-  for(int i = 0; i < melody_length; i++) {
-    pwm_set_frequency(PWMN(0), melody[i]);
-    pwm_enable(PWMN(0)); 
-  }
+  
+  pwm_set_frequency(PWMN(0), 25000000);
+  pwm_enable(PWMN(0)); 
+  
+  pwm_set_frequency(PWMN(0), 20000000);
+  pwm_enable(PWMN(0)); 
+  
+  pwm_set_frequency(PWMN(0), 15000000);
+  pwm_enable(PWMN(0)); 
+  
+  pwm_set_frequency(PWMN(0), 10000000);
+  pwm_enable(PWMN(0)); 
+  
+  pwm_set_frequency(PWMN(0), 5000000);
+  pwm_enable(PWMN(0)); 
+  
   MAIN_RETURN;
 }
