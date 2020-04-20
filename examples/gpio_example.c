6a6442ec0d37c1a3a1517a67cccac217a160a18e
rtswan22 <46530898+rtswan22@users.noreply.github.com>
2020-04-20 11:28:28 -0400

    Update gpio_example.c

--- examples/gpio_example.c
+++ examples/gpio_example.c
@@ -5,8 +5,11 @@
 // This demonstration would be better with a posedge interrupt but the interrupt handler has not been implemented.
 
 // 'digit' is aligned [][7:0] = [-, a, b, c, d, e, f, g] for the 7 segment display
-// 'digit': 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, b, C, d, E, f.
-unsigned int digit[] = {0b1111110, 0b01100000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1110011, 0b1110111, 0b0011111, 0b1001110, 0b0111101, 0b1001111, 0b1000111};
+// 'digit': 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, b, C, d, E, f
+unsigned int digit[] = {0b1111110, 0b1100000, 0b1101101, 0b1111001,
+                        0b0110011, 0b1011011, 0b1011111, 0b1110000,
+                        0b1111111, 0b1110011, 0b1110111, 0b0011111,
+                        0b1001110, 0b0111101, 0b1001111, 0b1000111};
 
 int main() {
   MAIN_INIT;
