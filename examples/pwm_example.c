64a2bdda0572f4788929c10d8c42dd6e69a36d4f
rtswan22 <46530898+rtswan22@users.noreply.github.com>
2020-04-23 14:07:02 -0400

    Update pwm_example.c

--- examples/pwm_example.c
+++ examples/pwm_example.c
@@ -4,15 +4,19 @@
 int main() {
   MAIN_INIT;
   
+  pwm_disable(PWMN(0));
   pwm_set_frequency(PWMN(0), 25000000);
   pwm_enable(PWMN(0));
   
+  pwm_disable(PWMN(0));
   pwm_set_frequency(PWMN(0), 12500000);
   pwm_enable(PWMN(0));
   
+  pwm_disable(PWMN(0));
   pwm_set_frequency(PWMN(0), 10000000);
   pwm_enable(PWMN(0));
   
+  pwm_disable(PWMN(0));
   pwm_set_frequency(PWMN(0), 5000000);
   pwm_enable(PWMN(0));
   
